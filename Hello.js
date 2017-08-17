a58ac567179ad05f58493d5627104e22f88c2d91
mcred2030 <red@enfre.com>
2017-08-17 16:05:55 +0900

    first Hello file

---
+++ Hello.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import config from './config.json';
+import styles from './app_style.css';
+
+
+class Hello extends Component {
+  render(){
+  	var textColor = "background-color:#000;"
+    return (
+      <div className={styles.rootcss}>
+      	{config.helloText}{textColor}
+      </div>
+    );
+  }
+}
+
+
+export default Hello
